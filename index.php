ea3f6f4749f200a4ef6fe38044e55e5a46f3fc55
khoi.van <khoi.van@tiki.vn>
2026-06-22 02:28:36 -0400

    php version 2 database

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
-echo "<h1>Version 1</h1>";
-echo "<p>CI/CD Demo</p>";
-?>
+echo "<h1>Version 2</h1>";
+$conn = new mysqli(
+    getenv("DB_HOST"),
+    getenv("DB_USER"),
+    getenv("DB_PASS"),
+    getenv("DB_NAME")
+);
+
+if ($conn->connect_error) {
+    die("Database connection failed");
+}
+
+echo "<h1>User List</h1>";
+
+$result = $conn->query("SELECT * FROM users");
+
+while ($row = $result->fetch_assoc()) {
+    echo $row["id"] . " - " . $row["name"] . "<br>";
+}
---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100)
+);
+
+INSERT INTO users(name) VALUES ('khoi');
+INSERT INTO users(name) VALUES ('hehe');
+INSERT INTO users(name) VALUES ('mike');
